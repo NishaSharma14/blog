7d77bede59d9e2a97c7f61175086ab43b73fd069
Nisha Sharma <sharmanis14@gmail.com>
2021-05-22 19:44:53 +0200

    database changes

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,8 @@
 <article>
 
     <h1>{{$post->title}}</h1>
-    By <a href="/authors/{{$post->author->id}}">{{$post->author->name}} </a> in <a href="/categories/{{ $post->category->slug}}">{{ $post->category->name}} </a>
+    By <a href="/authors/{{$post->author->id}}">{{$post->author->name}} </a> in <a href="/categories/{{ $post->category->slug}}">{{ $post->category->name}}</a>
+
     <div>
         {!! $post->body !!}
     </div>
